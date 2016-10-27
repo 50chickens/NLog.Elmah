94da64c15e0659e1dc93570e88d8680352379ccc
Alan Morrison <alan.morrison@taipannetworks.com.au>
2016-10-27 13:38:48 +1100

    added ip address of remote host as 'user', and also shows nlog as application name (to differentiate it from other log types in the dashboard).

--- NLog.Elmah/ElmahTarget.cs
+++ NLog.Elmah/ElmahTarget.cs
@@ -20,61 +20,63 @@ using NLog.Targets;
 
 namespace NLog.Elmah
 {
-    /// <summary>
-    /// Write messages to Elmah.
-    /// </summary>
-	[Target("Elmah")]
-	public sealed class ElmahTarget : TargetWithLayout
-	{
-		private readonly ErrorLog _errorLog;
+    [Target("Elmah")]
+    public sealed class ElmahTarget : TargetWithLayout
+    {
+        private readonly ErrorLog _errorLog;
 
-        /// <summary>
-        /// Use <see cref="LogEventInfo.Level"/> as type if <see cref="LogEventInfo.Exception"/> is <c>null</c>.
-        /// </summary>
-		public bool LogLevelAsType { get; set; }
+        public bool LogLevelAsType { get; set; }
 
-        /// <summary>
-        /// Target with default errorlog.
-        /// </summary>
-		public ElmahTarget()
-			: this(ErrorLog.GetDefault(null))
-		{ }
+        public ElmahTarget()
+            : this(ErrorLog.GetDefault(null))
+        { }
 
-        /// <summary>
-        /// Target with errorLog.
-        /// </summary>
-        /// <param name="errorLog"></param>
-		public ElmahTarget(ErrorLog errorLog)
-		{
-			_errorLog = errorLog;
-			LogLevelAsType = false;
-		}
+        public ElmahTarget(ErrorLog errorLog)
+        {
+            errorLog.ApplicationName = "Nlog";
+            _errorLog = errorLog;
+            LogLevelAsType = false;
+        }
 
-        /// <summary>
-        /// Write the event.
-        /// </summary>
-        /// <param name="logEvent">event to be written.</param>
-		protected override void Write(LogEventInfo logEvent)
-		{
-			var logMessage = Layout.Render(logEvent);
+        protected override void Write(LogEventInfo logEvent)
+        {
+            var logMessage = Layout.Render(logEvent);
 
-			var httpContext = HttpContext.Current;
-			var error = logEvent.Exception == null ? new Error() : httpContext != null ? new Error(logEvent.Exception, httpContext) : new Error(logEvent.Exception);
-			var type = error.Exception != null
-						   ? error.Exception.GetType().FullName
-						   : LogLevelAsType ? logEvent.Level.Name : string.Empty;
-			error.Type = type;
-			error.Message = logMessage;
-			error.Time = GetCurrentDateTime == null ? logEvent.TimeStamp : GetCurrentDateTime();
-			error.HostName = Environment.MachineName;
-			error.Detail = logEvent.Exception == null ? logMessage : logEvent.Exception.StackTrace;
+            var httpContext = HttpContext.Current;
+            var error = logEvent.Exception == null ? new Error() : httpContext != null ? new Error(logEvent.Exception, httpContext) : new Error(logEvent.Exception);
+            var type = error.Exception != null
+                           ? error.Exception.GetType().FullName
+                           : LogLevelAsType ? logEvent.Level.Name : string.Empty;
+            error.Type = type;
 
-			_errorLog.Log(error);
-		}
+            error.Message = logMessage;
+            error.Time = GetCurrentDateTime == null ? logEvent.TimeStamp : GetCurrentDateTime();
+            error.HostName = Environment.MachineName;
+            error.Detail = logEvent.Exception == null ? logMessage : logEvent.Exception.StackTrace;
+            if (error.User == "")
+            {
+                error.User = GetIP();
+            }
+            error.Source = logEvent.LoggerName;
+            _errorLog.Log(error);
+        }
 
-        /// <summary>
-        /// Method for retrieving current date and time. If <c>null</c>, then <see cref="LogEventInfo.TimeStamp"/> will be used.
-        /// </summary>
-		public Func<DateTime> GetCurrentDateTime { get; set; }
-	}
+        public Func<DateTime> GetCurrentDateTime { get; set; }
+        public static String GetIP()
+        {
+            String ip =
+                HttpContext.Current.Request.ServerVariables["HTTP_X_FORWARDED_FOR"];
+
+            if (string.IsNullOrEmpty(ip))
+            {
+                ip = HttpContext.Current.Request.ServerVariables["REMOTE_ADDR"];
+            }
+            if (ip.Contains(":"))
+            {
+                //ipaddress can be composed of ip:srcport. so grab just the ip component if so. 
+                return ip.Split(':')[0];
+            }
+            return ip;
+        }
+    }
 }
